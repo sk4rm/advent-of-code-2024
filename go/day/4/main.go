374e1e8967c8837b9a7acaf7c377e1d38edd43df
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2024-12-07 22:48:07 -0800

    Complete day 4 part 2 (rushed)

--- go/day/4/main.go
+++ go/day/4/main.go
@@ -12,6 +12,8 @@ import (
 var input string
 
 const match string = "XMAS"
+const part1 = false
+const part2 = true
 
 // Set during initializeSeeds()
 var (
@@ -46,35 +48,70 @@ func init() {
 		width = len(rows[0])
 	}
 
-	for i, row := range rows {
-		for j, cell := range row {
+	if part1 {
+		for i, row := range rows {
+			for j, cell := range row {
 
-			if byte(cell) != match[0] {
-				continue
-			}
-
-			// Eight directions
-			for dx := -1; dx <= 1; dx += 1 {
-				for dy := -1; dy <= 1; dy += 1 {
-
-					if dx == 0 && dy == 0 {
-						continue
-					}
-
-					seed := seed{j, i, dx, dy}
-					seeds = append(seeds, seed)
+				if byte(cell) != match[0] {
+					continue
 				}
-			}
 
+				// Eight directions
+				for dx := -1; dx <= 1; dx += 1 {
+					for dy := -1; dy <= 1; dy += 1 {
+
+						if dx == 0 && dy == 0 {
+							continue
+						}
+
+						seed := seed{j, i, dx, dy}
+						seeds = append(seeds, seed)
+					}
+				}
+
+			}
 		}
 	}
 }
 
 func main() {
-	for _, c := range match[1:] {
-		seeds = explore(seeds, c)
+	if part1 {
+		for _, c := range match[1:] {
+			seeds = explore(seeds, c)
+		}
+		fmt.Println(len(seeds))
 	}
-	fmt.Println(len(seeds))
+
+	if part2 {
+		Ai := []int{}
+		Aj := []int{}
+
+		for i, row := range rows {
+			if i == 0 || i == len(rows)-1 {
+				continue
+			}
+			for j, letter := range row {
+				if j == 0 || j == len(row)-1 {
+					continue
+				}
+
+				if letter == 'A' {
+					Ai = append(Ai, i)
+					Aj = append(Aj, j)
+				}
+
+			}
+		}
+
+		count := 0
+		for idx := range Ai {
+			if checkAll(Ai[idx], Aj[idx]) {
+				count += 1
+			}
+		}
+
+		fmt.Println(count)
+	}
 }
 
 func explore(seeds []seed, c rune) []seed {
@@ -111,4 +148,24 @@ func removeSeed(seeds []seed, idx int) []seed {
 	// https://stackoverflow.com/a/37335777
 	seeds[idx] = seeds[len(seeds)-1]
 	return seeds[:len(seeds)-1]
+}
+
+func checkAll(i, j int) bool {
+	return check1(i, j) || check2(i, j) || check3(i, j) || check4(i, j)
+}
+
+func check1(i, j int) bool {
+	return rows[i-1][j-1] == 'M' && rows[i-1][j+1] == 'M' && rows[i+1][j-1] == 'S' && rows[i+1][j+1] == 'S'
+}
+
+func check2(i, j int) bool {
+	return rows[i-1][j-1] == 'M' && rows[i-1][j+1] == 'S' && rows[i+1][j-1] == 'M' && rows[i+1][j+1] == 'S'
+}
+
+func check3(i, j int) bool {
+	return rows[i-1][j-1] == 'S' && rows[i-1][j+1] == 'S' && rows[i+1][j-1] == 'M' && rows[i+1][j+1] == 'M'
+}
+
+func check4(i, j int) bool {
+	return rows[i-1][j-1] == 'S' && rows[i-1][j+1] == 'M' && rows[i+1][j-1] == 'S' && rows[i+1][j+1] == 'M'
 }
